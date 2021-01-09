247bc217177dd7bb473f1deb5867aa0f7a31e54f
James <jamesgault1488@gmail.com>
2021-01-09 02:58:08 -0500

    updated html,css, script files... also updated readme

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# work-day-scheduler
+# Work-Day-Scheduler
+
+---
+
+<!-- Preview with live url goes here -->
+
+## Description
+
+This webpage displays a workday scheduler that can save the event or task to local storage.
+
+---
+
+## Developers
+
+James J. Gault
+
+---
+
+## Technologies
+
+1. HTML
+2. CSS
+3. JavaScript
+4. JQuery
+5. BootStrap
+6. Moment.JS
+
+---
+
+## Foresight
+
+The future features of this website will include the tasks popping up at the bottom of the website for client-side visual. An alert for the event should pop up when the time is reached.
+
+---
+
+## Incentive
+
+This website could help someone better manage their time.
+
+---
+
+## Challenges
+
+Some of the main challenges of this website came from managing and organizing the data.
+
+---
+
+## Reasoning
+
+This project was built as a means to show competence of having a webpage update with persistent data.
+
+---
+
+## Solution
+
+This webpage helps the user to enter in events and save them.
+
+---
+
+## Take away
+
+Persistent data allows delevopers to store data on the clients computer until it is needed. Developers can use this data for other things.
+
+---
--- script.js
+++ script.js
@@ -1,85 +1,219 @@
 // --------------------- task list for this file ----------------------------
-// change to htmlH1InnerText.innerHTML = DateTime.local(); to reflect the current time (FORMAT IT)
 
-let momentTime = moment();
-let htmlH1InnerText = document.querySelector("#html-h1");
+// ------------------------------------------------Setting Time---------------------------------------------------------------------
+// current time function
+let currentDayAndTime = function () {
+    // selects currentDay in html file and places the time in the elements space
+    $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-
-htmlH1InnerText.innerHTML = momentTime;
-
-
-console.log(momentTime.toString());
+}
 
 
-$(document).ready(function(){
-    // selects button from the dropdown menu and starts a function
-$('#time-button-1').on('click', () => {
-// allows user to toggle the menu button for a time selection
-    $('#hour-dropdown').toggleClass('show');
+// setInterval function to refresh the time so that it stays current
+// repeats the function currentDayAndTime every 1000 miliseconds
+setInterval(currentDayAndTime, 50);
+
+
+// ------------------------------------------------Code to load after document has fully loaded---------------------------------------------------------------------
+$(document).ready(function () {
+
+    // set saved task into local storage when the save button is pressed
+    $("#9am-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved9am = document.getElementById("nineAM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved9am)
+        localStorage.setItem('eventToBeSaved9am', serializedEventToBeSaved);
+
+    })
+
+    // set saved task into local storage when the save button is pressed
+    $("#10am-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved10am = document.getElementById("tenAM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved10am)
+        localStorage.setItem('eventToBeSaved10am', serializedEventToBeSaved);
+
+    })
+
+    // set saved task into local storage when the save button is pressed
+    $("#11am-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved11am = document.getElementById("elevenAM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved11am)
+        localStorage.setItem('eventToBeSaved11am', serializedEventToBeSaved);
+
+    })
+
+    // set saved task into local storage when the save button is pressed
+    $("#12pm-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved12pm = document.getElementById("twelvePM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved12pm)
+        localStorage.setItem('eventToBeSaved12pm', serializedEventToBeSaved);
+
+    })
+
+    // set saved task into local storage when the save button is pressed
+    $("#1pm-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved1pm = document.getElementById("onePM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved1pm)
+        localStorage.setItem('eventToBeSaved1pm', serializedEventToBeSaved);
+
+    })
+
+    // set saved task into local storage when the save button is pressed
+    $("#2pm-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved2pm = document.getElementById("twoPM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved2pm)
+        localStorage.setItem('eventToBeSaved2pm', serializedEventToBeSaved);
+
+    })
+
+    // set saved task into local storage when the save button is pressed
+    $("#3pm-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved3pm = document.getElementById("threePM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved3pm)
+        localStorage.setItem('eventToBeSaved3pm', serializedEventToBeSaved);
+
+    })
+
+    // set saved task into local storage when the save button is pressed
+    $("#4pm-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved4pm = document.getElementById("fourPM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved4pm)
+        localStorage.setItem('eventToBeSaved4pm', serializedEventToBeSaved);
+
+    })
+
+    // set saved task into local storage when the save button is pressed
+    $("#5pm-save").on('click', function () {
+        // desired result is for the function to store event-to-be-logged input value to local storage
+        let eventToBeSaved5pm = document.getElementById("fivePM-input").value;
+
+        let serializedEventToBeSaved = JSON.stringify(eventToBeSaved5pm)
+        localStorage.setItem('eventToBeSaved5pm', serializedEventToBeSaved);
+
+    })
+
+    // ---------------------------------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------
+
+    const rows = document.getElementsByClassName("row");
+    let currentHour = parseInt(moment().format('H'));
+
+    Array.from(rows).forEach(row => {
+        let
+            rowIdString = row.id,
+            rowHour;
+        if (rowIdString) {
+            rowHour = parseInt(rowIdString);
+        }
+        if (rowHour) {
+            // Compares row id to current hour and sets color accordingly
+            if (currentHour === rowHour) {
+                setColor(row, "red");
+            } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+                setColor(row, "green");
+            } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+                setColor(row, "lightgrey");
+            } else {
+                setColor(row, "white");
+            }
+        }
+    })
+
+    function setColor(element, color) {
+        element.style.backgroundColor = color;
+    }
 
 })
 
-//  selects the 9am - 10am button in the dropdown menu and starts a function
-$('#nineToTen').on('click', () => {
-    // when the 9am-10am button is clicked, ......
 
-    // hide the dropdown menu
-    $('#hour-dropdown').hide()
-    // add selection to the 'time selected' slot
-    $('#time-selected').append("9:00am - 10:00am");
-    // hide the 'select event time' button
-    $('#time-button-1').hide()
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 
-})
+    // let currentTime = $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    // var currentTime = moment().format("h")
+    // var currentHour = currentTime;
+    // var pastHours = pastHours < currentTime;
+    // var futureHours =  futureHours < currentTime;
+    // console.log(currentHour)
+    // console.log(pastHours)
+    // console.log(futureHours)
 
-$('#tenToEleven').on('click', () => {
-    // when the 10am-11am button is clicked.....
+    // if (currentTime == currentHour) {
+    //     // changed background color to yellow
 
-    // hide the dropdown menu
-    $('#hour-dropdown').hide();
-    // add the selected time to the 'time selected' slot
-    $('#time-selected').append("10:00am - 11:00am");
-    // hide the 'select event time' button
-    $('#time-button-1').hide();
+    //     $(".row").addClass("grey");
 
-})
+    // } else if (currentHour < currentTime) {
+    //     // change background color to red
 
-$('#elevenToTwelve').on('click', () => {
-    // when the 11am-12pm button is clicked.....
+    //     $(".row").addClass("red");
 
-    // hide the dropdown menu
-    $('#hour-dropdown').hide();
-    // add the selected time to the 'selected time' slot
-    $('#time-selected').append('11:00am - 12:00pm');
-    // hide the 'select event time' button
-    $('#time-button-1').hide();
+    // } else {
+    //     // change background color to green
+    //     $(".row").addClass("green");
 
-})
+    // }
 
-$('#twelveToOne').on('click', () => {
-    // when the 12pm-1pm button is clicked.....
+    // ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 
-    // hide the dropdown menu
-    $('#hour-dropdown').hide();
-    // add the selected time to the 'selected time' slot
-    $('#time-selected').append('12:00pm - 1:00pm');
-    // hide the 'select event time' button
-    $('#time-button-1').hide();
+// // make background color of / past hours gray / current hour red / future hours green
+// var hourPast = hourPast < currentTime;
+// var hourNow = currentTime;
+// var hourFuture = hourFuture < currentTime;
 
-})
+// // if currentTime > hourNow make background color gray//
 
-$('#oneToTwo').on('click', () => {
-    // when the 1pm-2pm button is clicked.....
-
-    // hide the dropdown menu
-    $('#hour-dropdown').hide();
-    // add the selected time to the 'selected time' slot
-    $('#time-selected').append('1:00pm - 2:00pm');
-    // hide the 'select event time' button
-    $('#time-button-1').hide();
-})
+// var hourNow = currentTime;
+// console.log(hourNow)
+// console.log(currentTime)
+//     function backgroundColorEdit(){
+//         if (hourNow == currentTime){
+//             document.getElementsByClassName("backgroundTool").style.backgroundColor = "#ff0000";
+//         }else if (hourNow < currentTime){
+//             document.getElementsByClassName("backgroundTool").style.backgroundColor = "#d3d3d3";
+//         }else 
+//         {
+//             document.getElementsByClassName("backgroundTool").style.backgroundColor = "#90ee90";
+//         }
+// }
 
 
+// function updateBackground() {
+//     var 
+//       hr = (new Date()).getHours(),
+//       body = document.body,
+//       bstyle = body.style,    
+//       hello = document.querySelector(".hello");    
+//     if (hr < 10) {
+//       bstyle.backgroundColor = "yellow";
+//       bstyle.color = "black";
+//       hello.innerText = "Have a good morning";
+//     } else if (hr < 20) {
+//       bstyle.backgroundColor = "green";
+//       bstyle.color = "white";
+//       hello.innerText ="Have a good day!";
+//     } else {
+//       bstyle.backgroundColor = "black";
+//       bstyle.color = "white";
+//       hello.innerText = "Have a good night!";
+//     } 
+//   }
 
-
-})
+//   setInterval(updateBackground, 1000 * 60);
+//   updateBackground();
